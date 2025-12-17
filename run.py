a2fbf65391b456a63ea58b67c2d94134023153e4
Brett Cannon <brett@python.org>
2025-12-17 11:48:41 -0800

    ruff format

--- run.py
+++ run.py
@@ -24,6 +24,7 @@ import sys
 # If changed, also update the module docstring.
 DEFAULT_CREDENTIALS_PATH = pathlib.Path("wasi-buildbot.env")
 
+
 def main() -> None:
     parser = argparse.ArgumentParser(description="Run the WASI buildbot container")
     parser.add_argument(
